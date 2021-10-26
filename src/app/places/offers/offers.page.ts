0e444bfa8ba8137fdd10aab5bb3a2a0810e11aac
David Munkacsi <david.munkacsi93@gmail.com>
2021-10-26 20:53:25 +0200

    Implement offers page.

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PlacesService } from 'src/app/places.service';
+import { Place } from '../places.model';
 
 @Component({
   selector: 'app-offers',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offers.page.scss'],
 })
 export class OffersPage implements OnInit {
+  places: Place[];
 
-  constructor() { }
+  constructor(private placesService: PlacesService) { }
 
   ngOnInit() {
+    this.places = this.placesService.places;
   }
-
 }
