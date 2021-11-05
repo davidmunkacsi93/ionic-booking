dc647e0762cf15a1165c8ad043cc65e9d7ea47ed
David Munkacsi <david.munkacsi93@gmail.com>
2021-11-05 15:16:58 +0100

    Add virtual scrolling for discover page.

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -10,14 +10,15 @@ import { Place } from '../places.model';
 })
 export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
+  listedLoadedPlaces: Place[];
 
   constructor(
     private placesService: PlacesService,
-    private menuCtrl: MenuController
   ) {}
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+    this.listedLoadedPlaces = this.loadedPlaces.slice(1);
   }
 
 }
