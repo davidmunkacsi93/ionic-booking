b5a5494e1942d52cfb0b427bca12f123c52ce170
David Munkacsi <david.munkacsi93@gmail.com>
2021-12-11 20:14:55 +0100

    Fetch offers from server.

--- src/app/places.service.ts
+++ src/app/places.service.ts
@@ -7,50 +7,29 @@ import { take, map, tap, delay, switchMap } from 'rxjs/operators';
 import { AuthService } from './auth/auth.service';
 import { Place } from './places/places.model';
 
+interface PlaceData {
+  dateFrom: string;
+  dateTo: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+  title: string;
+  userId: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
   private dbUrl =
     'https://ionic-booking-d33eb-default-rtdb.europe-west1.firebasedatabase.app/';
-  private _places = new BehaviorSubject<Place[]>([
-    new Place(
-      'p1',
-      'Manhattan Mansion',
-      'In the heart of NYC.',
-      'https://thumbs.nestseekers.com/JUlRoXyPxJK9R5XG.jpg',
-      149.99,
-      new Date(2021, 10, 12),
-      new Date(2021, 12, 14),
-      'abc'
-    ),
-    new Place(
-      'p2',
-      // eslint-disable-next-line @typescript-eslint/quotes
-      "L'Amour Toujours",
-      'A romantic place in paris.',
-      'https://images.squarespace-cdn.com/content/v1/5bc5dd89f8135a188f4d62a5/1583501077136-YWI0NDO0D0PXBI229SV4/ke17ZwdGBToddI8pDm48kIKSX_SZjyDOj8RLeXGW1RN7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmQyViSO8WVy1F2YAzXWvEVNabebp8wOGGQ-vpKUbL-vgz2y3-FwKjJA13dgL8WU6V/The+Beautiful+Paris+Apartment+of+Jackie+Kai+Ellis+-+The+Nordroom',
-      189.99,
-      new Date(2022, 1, 12),
-      new Date(2022, 2, 14),
-      'cde'
-    ),
-    new Place(
-      'p3',
-      'The Foggy Palace',
-      'Not your average city trip.',
-      'https://images.unsplash.com/photo-1606454277702-91f9c92257cc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9nZ3klMjBjaXR5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
-      99.99,
-      new Date(2021, 12, 14),
-      new Date(2022, 1, 11),
-      'abc'
-    ),
-  ]);
+  private offeredPlacesUrl = this.dbUrl + 'offered-places.json';
+  private $places = new BehaviorSubject<Place[]>([]);
 
   constructor(private authService: AuthService, private http: HttpClient) {}
 
   get places() {
-    return this._places.asObservable();
+    return this.$places.asObservable();
   }
 
   getPlace(id: string) {
@@ -58,6 +37,38 @@ export class PlacesService {
       take(1),
       map((places) => ({ ...places.find((p) => p.id === id) }))
     );
+  }
+
+  fetchPlaces() {
+    return this.http
+      .get<{ [key: string]: PlaceData }>(this.offeredPlacesUrl)
+      .pipe(
+        map((resData) => {
+          const places = [];
+          for (const key in resData) {
+            if (resData.hasOwnProperty(key)) {
+              const placeByKey = resData[key];
+              places.push(
+                new Place(
+                  key,
+                  placeByKey.title,
+                  placeByKey.description,
+                  placeByKey.imageUrl,
+                  placeByKey.price,
+                  new Date(placeByKey.dateFrom),
+                  new Date(placeByKey.dateTo),
+                  placeByKey.userId
+                )
+              );
+            }
+          }
+
+          return places;
+        }),
+        tap(places => {
+          this.$places.next(places);
+        })
+      );
   }
 
   addPlace(
@@ -83,7 +94,7 @@ export class PlacesService {
 
     let generatedId;
     return this.http
-      .post<{ name: string }>(this.dbUrl + 'offered-places.json', {
+      .post<{ name: string }>(this.offeredPlacesUrl, {
         ...newPlace,
         id: null,
       })
@@ -95,7 +106,7 @@ export class PlacesService {
         take(1),
         tap((places) => {
           newPlace.id = generatedId;
-          this._places.next(places.concat(newPlace));
+          this.$places.next(places.concat(newPlace));
         })
       );
   }
@@ -134,7 +145,7 @@ export class PlacesService {
           oldPlace.userId
         );
 
-        this._places.next(updatedPlaces);
+        this.$places.next(updatedPlaces);
       })
     );
   }
