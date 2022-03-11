9fc4ebbde4ff1a4aa5f75327939bfa67a054fa38
David Munkacsi <david.munkacsi93@gmail.com>
2022-03-11 15:18:07 +0100

    Add image preview.

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -14,6 +14,9 @@ import { of } from 'rxjs';
   styleUrls: ['./location-picker.component.scss'],
 })
 export class LocationPickerComponent implements OnInit {
+  selectedLocationImage: string;
+  isLoading = false;
+
   constructor(private modalCtrl: ModalController, private http: HttpClient) {}
 
   ngOnInit() {}
@@ -31,6 +34,7 @@ export class LocationPickerComponent implements OnInit {
           address: null,
           staticMapImageUrl: null,
         };
+        this.isLoading = true;
 
         this.getAddress(modalData.data.lat, modalData.data.lng).pipe(
           switchMap((address) => {
@@ -39,7 +43,8 @@ export class LocationPickerComponent implements OnInit {
           })
         ).subscribe(staticMapImageUrl => {
           pickedLocation.staticMapImageUrl = staticMapImageUrl;
-          console.log(pickedLocation);
+          this.selectedLocationImage = staticMapImageUrl;
+          this.isLoading = false;
         });
       });
       modalEl.present();
