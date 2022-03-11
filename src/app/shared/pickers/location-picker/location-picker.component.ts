834b9028752baad4e43373e4ce39b321df82d3a6
David Munkacsi <david.munkacsi93@gmail.com>
2022-03-11 14:58:37 +0100

    Fetch street view image for picked location.

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -4,7 +4,9 @@ import { ModalController } from '@ionic/angular';
 import { MapModalComponent } from '../../map-modal/map-modal.component';
 
 import { environment } from '../../../../environments/environment';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
+import { PlaceLocation } from 'src/app/places/location.model';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-location-picker',
@@ -23,9 +25,22 @@ export class LocationPickerComponent implements OnInit {
           return;
         }
 
-        this.getAddress(modalData.data.lat, modalData.data.lng).subscribe(
-          (address) => { console.log(address); }
-        );
+        const pickedLocation: PlaceLocation = {
+          lat: modalData.data.lat,
+          lng: modalData.data.lng,
+          address: null,
+          staticMapImageUrl: null,
+        };
+
+        this.getAddress(modalData.data.lat, modalData.data.lng).pipe(
+          switchMap((address) => {
+            pickedLocation.address = address;
+            return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14));
+          })
+        ).subscribe(staticMapImageUrl => {
+          pickedLocation.staticMapImageUrl = staticMapImageUrl;
+          console.log(pickedLocation);
+        });
       });
       modalEl.present();
     });
@@ -42,5 +57,11 @@ export class LocationPickerComponent implements OnInit {
         return geoData.results[1].formatted_address;
       })
     );
+  }
+
+  private getMapImage(lat: number, lng: number, zoom: number) {
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x300&maptype=roadmap
+    &markers=color:red%7Clabel:Place%7C${lat},${lng}
+    &key=${environment.googleMapsAPIKey}`;
   }
 }
