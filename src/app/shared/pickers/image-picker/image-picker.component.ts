9f4011239f4274f337c0b31cc7b2d57549dfc65e
David Munkacsi <david.munkacsi93@gmail.com>
2022-03-27 11:53:47 +0200

    Enable file picker fallback and image preview.

--- src/app/shared/pickers/image-picker/image-picker.component.ts
+++ src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { Capacitor } from '@capacitor/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-image-picker',
@@ -8,16 +9,27 @@ import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
   styleUrls: ['./image-picker.component.scss'],
 })
 export class ImagePickerComponent implements OnInit {
+  @ViewChild('filePicker') filePickerRef: ElementRef<HTMLInputElement>;
   @Output() imagePick = new EventEmitter<string>();
   selectedImage: string;
+  usePicker = false;
 
-  constructor() {}
+  constructor(private platform: Platform) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log(`Mobile: ${this.platform.is('mobile')}`);
+    console.log(`Hybrid: ${this.platform.is('hybrid')}`);
+    console.log(`iOS: ${this.platform.is('ios')}`);
+    console.log(`Android: ${this.platform.is('android')}`);
+    console.log(`Desktop: ${this.platform.is('desktop')}`);
+    if ((this.platform.is('mobile') && !this.platform.is('hybrid')) || this.platform.is('desktop')) {
+      this.usePicker = true;
+    }
+  }
 
   onPickImage() {
-    if (!Capacitor.isPluginAvailable('Camera')) {
-      console.log('Camera not available.');
+    if (!Capacitor.isPluginAvailable('Camera') || this.usePicker) {
+      this.filePickerRef.nativeElement.click();
       return;
     }
 
@@ -38,5 +50,20 @@ export class ImagePickerComponent implements OnInit {
         console.log(error);
         return false;
       });
+  }
+
+  onFileChosen(event: Event) {
+    const pickedFile = (event.target as HTMLInputElement).files[0];
+
+    if (!pickedFile) {
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      const dataUrl = fileReader.result.toString();
+      this.selectedImage = dataUrl;
+    };
+    fileReader.readAsDataURL(pickedFile);
   }
 }
