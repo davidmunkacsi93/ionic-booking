25e52df7efc54e6603c03e56f77ae859ce620a4b
David Munkacsi <david.munkacsi93@gmail.com>
2022-05-06 13:56:05 +0200

    Storing a token in memory.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,10 @@
 /* eslint-disable no-underscore-dangle */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { User } from './user.model';
 
 export interface AuthResponseData {
   kind: string;
@@ -17,8 +20,7 @@ export interface AuthResponseData {
   providedIn: 'root',
 })
 export class AuthService {
-  private _userIsAuthenticated = false;
-  private _userId = null;
+  private _user = new BehaviorSubject<User>(null);
 
   constructor(private http: HttpClient) {}
 
@@ -30,25 +32,58 @@ export class AuthService {
         password,
         returnSecureToken: true,
       }
-    );
+    )
+    .pipe(tap(this.setUserData.bind(this)));
   }
 
   get userIsAuthenticated() {
-    return this._userIsAuthenticated;
-  }
-
-  get userId() {
-    return this._userId;
-  }
-
-  login(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`,
-      { email, password }
+    return this._user.asObservable().pipe(
+      map((user) => {
+        if (user) {
+          return !!user.token;
+        } else {
+          return false;
+        }
+      })
     );
   }
 
+  get userId() {
+    return this._user.asObservable().pipe(
+      map((user) => {
+        if (user) {
+          return user.id;
+        } else {
+          return null;
+        }
+      })
+    );
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`,
+        { email, password }
+      )
+      .pipe(tap(this.setUserData.bind(this)));
+  }
+
   logout() {
-    this._userIsAuthenticated = false;
+    this._user.next(null);
+  }
+
+  private setUserData(userData: AuthResponseData) {
+    const expirationTime = new Date(
+      new Date().getTime() + +userData.expiresIn * 1000
+    );
+    this._user.next(
+      new User(
+        userData.localId,
+        userData.email,
+        userData.idToken,
+        expirationTime
+      )
+    );
   }
 }
