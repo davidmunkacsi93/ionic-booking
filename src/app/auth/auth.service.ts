4252b674ffc4d7987746d80a0e2fa8daccca0136
David Munkacsi <david.munkacsi93@gmail.com>
2022-05-06 12:54:54 +0200

    Implement signup mechanism.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,14 +1,37 @@
 /* eslint-disable no-underscore-dangle */
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  localId: string;
+  expiresIn: string;
+  registered?: boolean;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  private _userIsAuthenticated = true;
-  private _userId = 'abc';
+  private _userIsAuthenticated = false;
+  private _userId = null;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseAPIKey}`,
+      {
+        email,
+        password,
+        returnSecureToken: true,
+      }
+    );
+  }
 
   get userIsAuthenticated() {
     return this._userIsAuthenticated;
