534d0d1072bf317649a8c8516bbff207d44ccc46
David Munkacsi <david.munkacsi93@gmail.com>
2022-05-07 09:41:35 +0200

    Tweak on android and ios config.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.udemycourse.ionicbookmd',
   appName: 'ionic-booking',
   webDir: 'www',
   bundledWebRuntime: false
